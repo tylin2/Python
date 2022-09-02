5ac628fd2dc27cc9231e2c729d1d4924bea67701
Ting-Yeh Lin <s88432000@hotmail.com>
2022-09-02 18:01:35 -0400

    Finish Chapter 12

---
+++ book 2 - Automate the Boring Stuff whit Python 2nd Edition/chapter 12 - Web/cha12.py
@@ -0,0 +1,42 @@
+# Chapter 12
+'''
+1.
+    webbrowser has a open() is to open the URL
+    requests is to download the file and page
+    bs4 is to parse HTML
+    selenium will open the web and operate it
+2.
+    requests.get() can return a Response object and has a text to download the texts
+3.
+    raise_for_status()
+4.
+    status_code and HTTP code
+5.
+    wb
+6.
+    ctrl+u
+7.
+
+8.
+    '#main'
+9.
+    '.highlight'
+10.
+    'div div'
+11.
+    'button[value="favorite"]'
+12.
+    spam.getText()
+13.
+    linkElem.attrs()
+14.
+    from selenium import webdriver
+15.
+    return the first one elem or selet a list
+16.
+    click() and send_keys()
+17.
+    .submit()
+18.
+    browser.next(),browser.back(),browser.refresh()
+'''
